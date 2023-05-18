912e6ceea614dde05c388144ae9d253f0ce6f41c
stndaru <stefanus.ndaru@ui.ac.id>
2023-05-19 02:53:06 +0700

    Added icon in transfer history

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -122,6 +122,9 @@ export default function Home() {
                                 <Td>
                                     <div className='flex flex-col gap-y-2'>
                                         <div className='flex flex-row gap-x-2'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-2 stroke-red-700">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+                                            </svg>
                                             <Badge variant='outline' colorScheme='red'>
                                                 VA Payment
                                             </Badge>
@@ -136,6 +139,9 @@ export default function Home() {
                                 <Td>
                                     <div className='flex flex-col gap-y-2'>
                                         <div className='flex flex-row gap-x-2'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-2 stroke-green-700">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25" />
+                                            </svg>
                                             <Badge variant='outline' colorScheme='green'>
                                                 Top Up
                                             </Badge>
@@ -150,6 +156,9 @@ export default function Home() {
                                 <Td>
                                     <div className='flex flex-col gap-y-2'>
                                         <div className='flex flex-row gap-x-2'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-2 stroke-red-700">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+                                            </svg>
                                             <Badge variant='outline' colorScheme='red'>
                                                 Transfer
                                             </Badge>
