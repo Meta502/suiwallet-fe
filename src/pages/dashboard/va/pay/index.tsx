104b028711b630bc14fde8bc11ab04be16f336d7
Kevin <vnctkevin@gmail.com>
2023-05-21 22:20:09 +0700

    WIP integration with BE on VA creation and payment

--- src/pages/dashboard/va/pay/index.tsx
+++ src/pages/dashboard/va/pay/index.tsx
@@ -2,7 +2,8 @@ import { Inter } from 'next/font/google'
 import { useRouter } from "next/router";
 import Link from 'next/link';
 import { useAuth } from '@/contexts/auth';
-import { useForm } from 'react-hook-form';
+import { set, useForm } from 'react-hook-form';
+import { useState } from 'react';
 import React from 'react';
 import {
     Text,
@@ -22,6 +23,8 @@ export default function Home() {
     const router = useRouter()
     const toast = useToast()
     const { register, handleSubmit, formState: { errors } } = useForm()
+    const [confirmationData, setConfirmationData] = useState(null);
+    const [error, setError] = useState('');
 
     React.useEffect(() => {
         if (!token) {
@@ -29,24 +32,32 @@ export default function Home() {
         }
     }, [router, token])
 
-    const handlePayment = (data:any) => {
+    
+
+    const handlePayment = async (data:any) => {
         //submit a PUT request to backend in process.env.NEXT_PUBLIC_BASE_API_URL/transaction/virtual-account/{vaId}
         //render the payment confirmation page with the response
         //confirm payment
         //use toast to display success message
-        axios.put(process.env.NEXT_PUBLIC_BASE_API_URL+ "/transaction/virtual-account/" + data.vaId, {
-        }
-        ).then((res) => {
+        try {
+            const res = await axios.get(process.env.NEXT_PUBLIC_BASE_API_URL+ "/transaction/virtual-account/" + data.vaId, {}
+            )
             console.log(res);
+            setConfirmationData(res.data);
+        } catch (e) {
+            setError(e.response.data.message);
+            toast({
+                title: "VA Payment Failed.",
+                description: "Your VA has not been paid.",
+                status: "error",
+                isClosable: true,
+            })
         }
-        ).catch((error) => {
-            console.log(error);
-        }
-        )
     }
 
     
     return (
+        <>
         <main className={`flex min-h-screen flex-col p-8 md:p-24 space-y-6 ${inter.className}`}>
             <div className='flex flex-col gap-y-8 md:flex-row md:justify-between'>
                 <div className='flex flex-col md:flex-row gap-x-4 gap-y-4'>
@@ -71,8 +82,40 @@ export default function Home() {
                     <Button type="submit" marginTop={4} colorScheme="blue">Pay</Button>
                 </form>
                 </Box>
+            
+            {confirmationData && (
+                <Box mx="auto" mt={8} borderWidth="1px" borderRadius="lg" p={8}>
+                    <div className="flex flex-col justify-center space-y-2 py-2">
+                        <label htmlFor="id">ID</label>
+                        <Box mb={4}>
+                            <Input type="text" id="id" value={confirmationData.id} disabled />
+                        </Box>
+                        </div>
+                    <div className="flex flex-col justify-center space-y-2 py-2">
+                        <label htmlFor="payment_id">Payment ID</label>
+                        <Box mb={4}>
+                            <Input type="text" id="payment_id" value={confirmationData.payment_id} disabled />
+                        </Box>
+                        </div>
+                    <div className="flex flex-col justify-center space-y-2 py-2">
+                        <label htmlFor="amount">Amount</label>
+                        <Box mb={4}>
+                            <Input type="text" id="amount" value={confirmationData.amount} disabled />
+                        </Box>
+                        </div>
+                    <div className="flex flex-col justify-center space-y-2 py-2">
+                        <label htmlFor="remaining_balance">Remaining Balance</label>
+                        <Box mb={4}>
+                            <Input type="text" id="remaining_balance" value={confirmationData.remaining_balance} disabled />
+                        </Box>
+                        </div>
+                    </Box>
+            )}
         </main>
+        </>
     )
 }
+
+
 
 
